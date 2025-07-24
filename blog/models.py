a3fd298707121521c10e08bc7255663a9cfc70f7
emanuelsoares97 <tutasoares17@gmail.com>
2025-07-24 17:05:09 +0100

    Database and Migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100) # título do post
+    content = models.TextField() # conteúdo do post
+    created_at = models.DateTimeField(default=timezone.now)    # data de criação do post
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #se o user for eliminado, os posts dele também serão removidos
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
-
-posts=[
-    {
-        'author': 'John Doe',
-        'title': 'My First Post',
-        'content': 'This is the content of my first post.',
-        'date_posted': 'August 27, 2023'
-    },
-    {
-        'author': 'Jane Smith',
-        'title': 'Another Day in Paradise',
-        'content': 'Today was a beautiful day in paradise.',
-        'date_posted': 'August 28, 2023'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     
