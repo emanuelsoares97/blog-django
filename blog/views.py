fb8d8b585d28cefb6008c025a4a4dc3e219d0817
emanuelsoares97 <tutasoares17@gmail.com>
2025-07-24 12:54:49 +0100

    criada as rotas basicas e estrutura de views

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Hello, world!</h1><p>Welcome to the blog homepage.</p>")
+
+def about(request):
+    return HttpResponse("<h1>About Us</h1><p>This is the about page of the blog.</p>")
