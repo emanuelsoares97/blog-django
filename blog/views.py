e5b87862b749463ead62732c7003ffbf249ec274
emanuelsoares97 <tutasoares17@gmail.com>
2025-07-24 14:08:46 +0100

    criados os templates base, home, about

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts=[
+    {
+        'author': 'John Doe',
+        'title': 'My First Post',
+        'content': 'This is the content of my first post.',
+        'date_posted': 'August 27, 2023'
+    },
+    {
+        'author': 'Jane Smith',
+        'title': 'Another Day in Paradise',
+        'content': 'Today was a beautiful day in paradise.',
+        'date_posted': 'August 28, 2023'
+    }
+]
 
 def home(request):
-    return HttpResponse("<h1>Hello, world!</h1><p>Welcome to the blog homepage.</p>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>About Us</h1><p>This is the about page of the blog.</p>")
+    
+    return render(request, 'blog/about.html', {'title': 'About'})
