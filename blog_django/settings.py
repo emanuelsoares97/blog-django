c99a201ac9c96552d36cefdd966302924c2e17cd
emanuelsoares97 <tutasoares17@gmail.com>
2025-08-18 13:06:09 +0100

    settings corrigido debug false no dominio render

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -63,12 +63,6 @@ GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
