93aa3ded11c966956a7371e2dde522069238932f
emanuelsoares97 <tutasoares17@gmail.com>
2025-08-19 11:45:53 +0100

    cloudinary esta com url incorreto tentativa de corrigir ao adicionar configuraçao no init

---
+++ blog_django/__init__.py
@@ -0,0 +1,10 @@
+import cloudinary
+
+from django.conf import settings
+
+cloudinary.config(
+cloud_name=settings.CLOUDINARY_STORAGE["CLOUD_NAME"],
+api_key=settings.CLOUDINARY_STORAGE["API_KEY"],
+api_secret=settings.CLOUDINARY_STORAGE["API_SECRET"],
+secure=True,
+)
